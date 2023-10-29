8f3e163e3d0972954481bee1022570fc49b6b7b6
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-10-30 02:33:45 +0600

    initial commit

---
+++ app/src/main/java/kz/just_code/viewpager/CustomPageTransformer.kt
@@ -0,0 +1,38 @@
+package kz.just_code.viewpager
+
+
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2.PageTransformer
+import kotlin.math.abs
+
+class CustomPageTransformer :PageTransformer {
+    override fun transformPage(page: View, position: Float) {
+        val absPosition = abs(position)
+
+        val pageWidth = page.width
+        val scaleFactor = 0.85f
+
+        // Если страница справа (свайп влево)
+        if (position >= 0) {
+            page.alpha = 1f
+            page.scaleX = 1 - absPosition * (1 - scaleFactor)
+            page.scaleY = 1 - absPosition * (1 - scaleFactor)
+            page.translationX = 0f
+        }
+        // Если страница слева (свайп вправо)
+        else {
+            page.alpha = 1f
+            page.scaleX = 1 - absPosition * (1 - scaleFactor)
+            page.scaleY = 1 - absPosition * (1 - scaleFactor)
+            // Смещение вправо, чтобы страница выходила поверх
+            page.translationX = pageWidth * absPosition
+        }
+    }
+        }
+
+
+
+
+
+
+
